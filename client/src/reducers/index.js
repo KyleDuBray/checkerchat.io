8f6531d580bed627069aa9e7da1e32848999b420
DuBrowski <krdubray@comcast.net>
2021-07-12 22:25:17 -0500

    added core reducer boilerplate for redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import gameReducer from "./gameReducer";
+import userReducer from "./userReducer";
+import chatReducer from "./chatReducer";
 
 export default combineReducers({
-  game: '',
+  game: gameReducer,
+  user: userReducer,
+  chat: chatReducer,
 });
