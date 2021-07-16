2c0703bdb95ff0d71986df6a9690de0113d47d17
Kyle <krdubray@comcast.net>
2021-07-16 18:36:03 -0500

    added redux auth

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from "redux";
-import gameReducer from "./gameReducer";
-import userReducer from "./userReducer";
-import chatReducer from "./chatReducer";
+import { combineReducers } from 'redux';
+import gameReducer from './gameReducer';
+import userReducer from './userReducer';
+import chatReducer from './chatReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
   game: gameReducer,
   user: userReducer,
   chat: chatReducer,
+  auth: authReducer,
 });
